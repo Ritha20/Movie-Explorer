3ecfecfe529f7072045f7aae4ed5edc6a8ba745d
Irakoze Ritha Theonestine <irakozeritha20@gmail.comclear>
2025-10-23 10:19:19 +0200

    created the functionality in app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    
+      <div className="min-h-screen bg-dark text-white">
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            
+          </Routes>
+        </main>
+      </div>
+    
+  )
+}
+
+export default App
