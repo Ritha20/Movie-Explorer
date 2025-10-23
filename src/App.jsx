606e055589829cdecaf8c36a17bec21f5c819bf7
Irakoze Ritha Theonestine <irakozeritha20@gmail.comclear>
2025-10-23 10:41:37 +0200

    edited the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import MovieDetails from './pages/movieDetails'
 import Favorites from './pages/Favorites'
-
+import { FavoritesProvider } from './Hooks/useFavorites'
 
 function App() {
   return (
-    
+    <FavoritesProvider>
       <div className="min-h-screen bg-dark text-white">
         <Navbar />
         <main className="container mx-auto px-4 py-8">
           <Routes>
-            
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:id" element={<MovieDetails />} />
+            <Route path="/favorites" element={<Favorites />} />
           </Routes>
         </main>
       </div>
-    
+    </FavoritesProvider>
   )
 }
 
